2801130bfee0acd579895891e106d3268f241a39
Serhiy <sergiyclas@gmail.com>
2025-10-03 21:18:24 +0300

    Show Healthcheck status during deploy

--- config/config.py
+++ config/config.py
@@ -1,18 +1,17 @@
-import yaml
+import os
 import mysql.connector
 
 def load_db_config():
-    with open('config/app.yml', 'r') as file:
-        config = yaml.safe_load(file)
-        return config['database']
+    """Зчитує конфігурацію бази з змінних середовища"""
+    return {
+        'host': os.getenv('DB_HOST'),
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWORD'),
+        'database': os.getenv('DB_NAME'),
+        'port': int(os.getenv('DB_PORT', 3306))  # дефолт 3306
+    }
 
 def connect_to_db():
     db_config = load_db_config()
-    connection = mysql.connector.connect(
-        host=db_config['host'],
-        user=db_config['user'],
-        password=db_config['password'],
-        database=db_config['db'],
-        port=db_config['port']
-    )
+    connection = mysql.connector.connect(**db_config)
     return connection
